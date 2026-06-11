a7c4ca5882df43a1812e75be73c2ebd00b17bdde
SP-MICOM <superfamicom1022@gmail.com>
2026-06-12 01:11:45 +0900

    (2026.6.11) 포톤 네트워크 설치

---
+++ Program/Assets/Scripts/Managers/PlayFabManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Photon.Pun;
+using PlayFab.ClientModels;
+using TMPro;
+public class PlayFabManager : MonoBehaviour
+{
+    [SerializeField] string version;
+    [SerializeField] TMP_InputField addressInputField;
+    [SerializeField] TMP_InputField passwordInputField;
+}
