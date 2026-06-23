d86e0d085e4a424de2c194e66b187487d6505b3c
SP-MICOM <superfamicom1022@gmail.com>
2026-06-23 21:43:23 +0900

    (2026.6.23) 룸 설정 시스템

--- Program/Assets/Scripts/UserInterfaces/RoomPanel.cs
+++ Program/Assets/Scripts/UserInterfaces/RoomPanel.cs
@@ -2,13 +2,52 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using UnityEngine.UI;
 using System;
 using Unity.VisualScripting;
 
 public class RoomPanel : MonoBehaviourPunCallbacks
 {
+    [SerializeField] int personnel = 0;
+    [SerializeField] Toggle[] toggles;
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] Button CreateRoomButton;
+
+    public void Start()
+    {
+        Select();
+    }
+
     public void CreateRoom()
     {
         RoomOptions roomOptions = new RoomOptions();
+
+        roomOptions.MaxPlayers = personnel;
+
+        roomOptions.IsOpen = true;
+
+        roomOptions.IsVisible = true;
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text, roomOptions);
+
+        gameObject.SetActive(false);
+    }
+
+    public void OnRoomNameChanged()
+    {
+        CreateRoomButton.interactable = string.IsNullOrWhiteSpace(roomNameInputField.text) == false;
+    }
+
+    public void Select()
+    {
+        for(int i = 0; i < toggles.Length; i++)
+        {
+            if (toggles[i].isOn)
+            {
+                personnel = i + 2;
+
+                break;
+            }
+        }
     }
 }
